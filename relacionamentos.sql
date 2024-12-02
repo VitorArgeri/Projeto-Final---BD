2670edf3b930e7ff72b355cf7f4e4fcb9b414f88
Pedro Santos <pedro.o.santos7@aluno.senai.br>
2024-12-02 19:01:38 -0300

    first querry

--- relacionamentos.sql
+++ relacionamentos.sql
@@ -14,12 +14,13 @@ CREATE TABLE autores (
 -- Criação da Tabela "Notícias"
 CREATE TABLE noticias (
     id SERIAL PRIMARY KEY,
-    CONSTRAINT fk_autor FOREIGN KEY (ID_autor) REFERENCES autores(ID_autor),
+    autor INT,
     titulo VARCHAR(500) NOT NULL,
     fonte VARCHAR(500) UNIQUE NOT NULL,
     lead VARCHAR(500) NOT NULL,
     data_publicacao DATE,
-    imagem BYTEA
+    imagem BYTEA,
+    CONSTRAINT fk_autor FOREIGN KEY (autor) REFERENCES autores(ID_autor)
 );
 
 -- Criação da Tabela "Cargos Scrum"
@@ -36,7 +37,7 @@ CREATE TABLE membros (
     cargo INT,
     email VARCHAR(150) UNIQUE NOT NULL,
     github VARCHAR(100),
-    CONSTRAINT fk_cargo FOREIGN KEY (cargo) REFERENCES cargos_scrum(cargo)
+    CONSTRAINT fk_cargo FOREIGN KEY (cargo) REFERENCES cargos_scrum(id)
 );
 
 -- Criação da Tabela "Entrevistados"
@@ -49,9 +50,11 @@ CREATE TABLE entrevistados (
 -- Criação da Tabela "Entrevistas"
 CREATE TABLE entrevistas (
     id SERIAL PRIMARY KEY,
-    CONSTRAINT fk_entrevistado FOREIGN KEY (entrevistado_id) REFERENCES entrevistados(entrevistado_id),
-    CONSTRAINT fk_cargo FOREIGN KEY (membro_id) REFERENCES membros(membro_id),
-    tempo_de_duracao DECIMAL(4, 2)
+    entrevistado INT,
+    entrevistador INT,
+    tempo_de_duracao DECIMAL(4, 2),
+    CONSTRAINT fk_entrevistado FOREIGN KEY (entrevistado) REFERENCES entrevistados(entrevistado_id),
+    CONSTRAINT fk_entrevistador FOREIGN KEY (entrevistador) REFERENCES membros(membro_id)
 );
 
 -- Criação da Tabela "Estágios"
@@ -61,16 +64,17 @@ CREATE TABLE estagios (
     empresa VARCHAR(100),
     salario_medio DECIMAL(4,2),
     requisitos_estagio VARCHAR(100),
-    carga_horaria INTEGER(2,3)
+    carga_horaria DECIMAL(2,3)
 );
 
 -- Criação da Tabela "Carreiras"
 CREATE TABLE carreiras (
     id_carreira SERIAL PRIMARY KEY,
     cargo_carreira VARCHAR(100),
-    estagio VARCHAR(100),
-    requisitos_cargo VARCHAR(100)
-)
+    estagio INT,
+    requisitos_cargo VARCHAR(100),
+    CONSTRAINT fk_estagio FOREIGN KEY (estagio) REFERENCES estagios(id_estagio)
+);
 
 -- Criação da Tabela "Simulados"
 CREATE TABLE simulados (
@@ -80,6 +84,75 @@ CREATE TABLE simulados (
 );
 
 --------------------------------Inserções de conteúdo--------------------------------
+
+-- Inserções na Tabela de "Autores"
+INSERT INTO autores (nome, contratante) VALUES
+('Miguel Sarti', 'SESI-SENAI'),
+('Marília Monitchele', 'Veja'),
+('Laísa Dall´Agnol', 'Veja'),
+('Wilson Ledo', 'CNN'),
+('Lucas Pordeus León', 'CNN'),
+('Mirella Cordeiro', 'CNN'),
+('Ana Zimmerman', 'G1'),
+
+-- Inserções na Tabela de "Notícias"
+INSERT INTO noticias (autor, titulo, fonte, lead, data_publicacao, imagem) VALUES 
+(1, ' Premiê de Portugal defende que português seja língua oficial da ONU', 'https://veja.abril.com.br/mundo/premie-de-portugal-defende-que-portugues-seja-lingua-oficial-da-onu/
+', 'Luis Montenegro abordou Lula e disse que o petista tem carinho pela pauta; Ele também reforçou candidatura de seu país ao Conselho de Segurança.', '2024-09-26', LOAD_FILE()),
+(2, 'Fernando Venâncio: A língua portuguesa não nasceu com os portugueses', 'https://https://veja.abril.com.br/educacao/fernando-venancio-a-lingua-portuguesa-nao-nasceu-com-os-portugues/
+-seja-lingua-oficial-da-onu/', 'Linguista português desfaz preconceitos e parte da formação das palavras para falar sobre a construção do idioma', '2024-05-28'),
+(3, 'Prefeitura de São Paulo quer guarani como segunda língua oficial da cidade 
+', 'https://veja.abril.com.br/coluna/radar/prefeitura-de-sao-paulo-quer-guarani-como-segunda-lingua-oficial-da-cidade/
+', 'O prefeito de São Paulo Ricardo Nunes (MDB) enviou à Câmara Municipal um projeto para tornar a língua guarani o idioma secundário da cidade.', '2021-09-12'),
+(4, 'Novas regras de Portugal para imigrantes geram dúvidas, entenda:', 'https://www.cnnbrasil.com.br/internacional/novas-regras-de-portugal-para-imigrantes-geram-duvidas-entenda/', 'Governo português anunciou plano que dificulta entrada no país; medida exigirá contrato de trabalho e fim das manifestações de interesse', '2024-06-04'),
+('Lucas Pordeus León', 'CPLP: entenda o que é a Comunidade dos Países de Língua Portuguesa.', 'https://www.cnnbrasil.com.br/internacional/cplp-entenda-o-que-e-a-comunidade-dos-paises-de-lingua-portuguesa/'
+, 'Integram a Comunidade dos Países de Língua Portuguesa (CPLP) países que, ao longo de sua história, foram colonizados pelo antigo Império Português.
+', '2023-08-27'),
+(5, '4 professores de português para seguir nas redes sociais.', 'https://www.cnnbrasil.com.br/educacao/5-professores-de-portugues-para-seguir-nas-redes-sociais/'
+, 'Com bom humor, professores ensinam português nas redes sociais de forma descomplicada.', '2024-09-21'),
+(1, 'Pesquisadores dedicados à linguística africana estudam seu impacto no Brasil de hoje.', 'https://jornal.usp.br/diversidade/pesquisadores-dedicados-a-linguistica-africana-estudam-impacto-no-brasil-de-hoje/'
+, 'Simpósio gratuito na USP reúne os primeiros docentes do Brasil a ocuparem cadeiras dedicadas ao estudo da linguística africana no país', '2024-08-26'),
+(1, 'MEC lança estratégia para melhorar alfabetização', 'https://www.soportugues.com.br/secoes/new.php?indice=174'
+, 'Após a divulgação dos dados do Censo da Educação Básica 2015, se comprometeu a unir esforços de três programas do governo para garantir a alfabetização do ensino fundamental.', '2016-03-23'),
+(1, 'Como usar o gerúndio corretamente sem virar gerundismo', 'https://exame.com/hub-faculdade-exame/como-usar-o-gerundio-corretamente-sem-virar-gerundismo-2/'
+, 'É correto usar o gerúndio quando se quer expressar uma ideia ou ação que ocorre no momento de outra no futuro', '2016-03-23'),
+(1, 'Projeto estudantil estimula aprendizagem da língua portuguesa e exercício da cidadania', 'https://jornal.usp.br/diversidade/projeto-estudantil-estimula-aprendizagem-da-lingua-portuguesa-e-exercicio-da-cidadania/'
+, 'Nos dias 10, 11 e 12 agosto, será possível se inscrever no Projeto Redigir, curso que capacita estudantes a desenvolver habilidades de comunicação, além de incentivar o pensamento crítico sobre direitos e deveres sociais.', '2023-08-10'),
+(1, 'Professora da USP Ribeirão oferece curso gratuito de português para imigrantes', 'https://jornal.usp.br/campus-ribeirao-preto/professora-da-usp-ribeirao-oferece-curso-gratuito-de-portugues-para-imigrantes/ua-portuguesa-e-exercicio-da-cidadania/'
+, 'As inscrições vão de 1º a 5 de agosto e o curso terá início dia 12 de agosto na Biblioteca Sinhá Junqueira, no centro de Ribeirão Preto.', '2023-07-20'),
+(1, 'Quantos países falam português no mundo além do Brasil?', 'https://exame.com/pop/quantos-paises-falam-portugues-no-mundo-alem-do-brasil-2/'
+, 'Junho, dia 10, é o Dia da Língua Portuguesa no Brasil, mas estamos longe de ser o único país a falar a língua', '2023-07-20'),
+(6, 'Série do Jornal Hoje aborda a língua portuguesa coloquial falada nas ruas', 'https://g1.globo.com/jornal-hoje/noticia/2015/03/serie-do-jornal-hoje-fala-sobre-lingua-coloquial-falada-nas-ruas.html'
+, 'A língua que a gente fala aborda a língua falada em conversas espontâneas.
+Linguistas defendem que na comunicação falada não existe certo ou errado.', '2015-03-18'),
+(1, 'Estudiosos discutem a língua portuguesa na educação, literatura e comunicação', 'https://www.soportugues.com.br/secoes/new.php?indice=174'
+, 'Evento de valorização da língua está recebendo trabalhos acadêmicos até 16 de agosto', '2019-07-30');
+
+-- Inserções na Tabela de "Cargos Scrum"
+INSERT INTO cargos_scrum (cargo, funcao) VALUES
+('Product Owner', 'Responsável por maximizar o valor do produto, gerenciar o backlog do produto e definir prioridades garantindo que a equipe de desenvolvedores entenda claramente os itens do backlog.'),
+('Scrum Master', 'Responsável por garantir que a equipe de desenvolvedores siga os princípios e práticas do Srum, removendo impedimentos, facilitando reuniões e promovendo melhorias.'),
+('Developers', 'Responsáveis por criar e entregar incrementos de produto de alta qualidade, colaborando para planejar, desenvolver, testar e revisar o trabalho necessário para atingir objetivos do sprint.');
+
+-- Inserções na Tabela de "Membros"
+INSERT INTO membros (nome, cargo, email, github) VALUES
+('Pedro de Oliveira', 1, 'pedro.o.santos7@aluno.senai.br', 'pedro-ols'),
+('Vitor Argeri', 2, 'vitor.argeri@aluno.senai.br', 'VitorArgeri'),
+('Miguel Sarti', 3, 'miguel.sarti@aluno.senai.br', 'miguelsarti'),
+('Raphaelle Lacerda', 3, 'raphaelle.lacerda@aluno.senai.br', 'R4phaLacerda'),
+('Sara Souza', 3, 'sara.e.souza9@aluno.senai.br', 's0uzx');
+
+-- Inserções na Tabela de "Entrevistados"
+INSERT INTO entrevistados (nome, cargo_rede_sesi) VALUES
+('Daniela Real','Professor de Português'),
+('Ivonete Pereira', 'Professor de Português'),
+('Ana Freire', 'Professor de Português');
+
+-- Inserções na Tabela de "Entrevistas"
+INSERT INTO entrevistas (entrevistado, entrevistador, tempo_de_duracao) VALUES
+(1, 1, 09.35),
+(2, 1, 10.13),
+(3, 1, 07.54);
 
 -- Inserções na Tabela de "Carreiras"
 INSERT INTO carreiras (cargo_carreira, estagio, requisitos_cargo) VALUES
@@ -94,80 +167,16 @@ INSERT INTO estagios (cargo_estagio, empresa, salario_medio, requisitos_estagio,
 ('Pedagogia', 'Pedagogia para Liberdade', '1706,71', 'Inglês Fluente, Educação, Conhecimento para Ensinar', '3.200'),
 ('Redação', 'Estagiário de Redação de Conteúdo', '1033,00', 'Graduação, Inglês fluente, Interpretação', '3.700'),
 
--- Inserções na Tabela de "Cargos Scrum"
-INSERT INTO cargos_scrum (cargo, funcao)
-('Product Owner', 'Responsável por maximizar o valor do produto, gerenciar o backlog do produto e definir prioridades garantindo que a equipe de desenvolvedores entenda claramente os itens do backlog.'),
-('Scrum Master', 'Responsável por garantir que a equipe de desenvolvedores siga os princípios e práticas do Srum, removendo impedimentos, facilitando reuniões e promovendo melhorias.'),
-('Developers', 'Responsáveis por criar e entregar incrementos de produto de alta qualidade, colaborando para planejar, desenvolver, testar e revisar o trabalho necessário para atingir objetivos do sprint.');
-
--- Inserções na Tabela de "Membros"
-INSERT INTO membros (nome, cargo, email, github)
-('Pedro de Oliveira', 1, 'pedro.o.santos7@aluno.senai.br', 'pedro-ols'),
-('Vitor Argeri', 2, 'vitor.argeri@aluno.senai.br', 'VitorArgeri'),
-('Miguel Sarti', 3, 'miguel.sarti@aluno.senai.br', 'miguelsarti'),
-('Raphaelle Lacerda', 3, 'raphaelle.lacerda@aluno.senai.br', 'R4phaLacerda'),
-('Sara Souza', 3, 'sara.e.souza9@aluno.senai.br', 's0uzx');
-
--- Inserções na Tabela de "Entrevistas"
-INSERT INTO entrevistas (entrevistado, tempo_de_duracao) VALUES
-('Daniela Real', 09.35),
-('Ivonete Pereira', 10.13),
-('Ana Freire', 07.54);
-
--- Inserções na Tabela de "Entrevistados"
-INSERT INTO entrevistados (nome, cargo) VALUES
-('Daniela Real','Professor de Português'),
-('Ivonete Pereira', 'Professor de Português'),
-('Ana Freire', 'Professor de Português');
-
--- Inserções na Tabela de "Notícias"
-INSERT INTO noticias (autor, titulo, fonte, lead, data_publicacao, imagem) VALUES 
-('Miguel Sarti', ' Premiê de Portugal defende que português seja língua oficial da ONU', 'https://veja.abril.com.br/mundo/premie-de-portugal-defende-que-portugues-seja-lingua-oficial-da-onu/
-', 'Luis Montenegro abordou Lula e disse que o petista tem carinho pela pauta; Ele também reforçou candidatura de seu país ao Conselho de Segurança.', '2024-09-26', LOAD_FILE()),
-('Marília Monitchele', 'Fernando Venâncio: A língua portuguesa não nasceu com os portugueses', 'https://https://veja.abril.com.br/educacao/fernando-venancio-a-lingua-portuguesa-nao-nasceu-com-os-portugues/
--seja-lingua-oficial-da-onu/', 'Linguista português desfaz preconceitos e parte da formação das palavras para falar sobre a construção do idioma', '2024-05-28'),
-('Laísa Dall Agnol', 'Prefeitura de São Paulo quer guarani como segunda língua oficial da cidade 
-', 'https://veja.abril.com.br/coluna/radar/prefeitura-de-sao-paulo-quer-guarani-como-segunda-lingua-oficial-da-cidade/
-', 'O prefeito de São Paulo Ricardo Nunes (MDB) enviou à Câmara Municipal um projeto para tornar a língua guarani o idioma secundário da cidade.', '2021-09-12'),
-('Wilson Ledo', 'Novas regras de Portugal para imigrantes geram dúvidas, entenda:', 'https://www.cnnbrasil.com.br/internacional/novas-regras-de-portugal-para-imigrantes-geram-duvidas-entenda/', 'Governo português anunciou plano que dificulta entrada no país; medida exigirá contrato de trabalho e fim das manifestações de interesse', '2024-06-04'),
-('Lucas Pordeus León', 'CPLP: entenda o que é a Comunidade dos Países de Língua Portuguesa.', 'https://www.cnnbrasil.com.br/internacional/cplp-entenda-o-que-e-a-comunidade-dos-paises-de-lingua-portuguesa/'
-, 'Integram a Comunidade dos Países de Língua Portuguesa (CPLP) países que, ao longo de sua história, foram colonizados pelo antigo Império Português.
-', '2023-08-27'),
-('Mirella Cordeiro', '4 professores de português para seguir nas redes sociais.', 'https://www.cnnbrasil.com.br/educacao/5-professores-de-portugues-para-seguir-nas-redes-sociais/'
-, 'Com bom humor, professores ensinam português nas redes sociais de forma descomplicada.', '2024-09-21'),
-('Miguel Sarti', 'Pesquisadores dedicados à linguística africana estudam seu impacto no Brasil de hoje.', 'https://jornal.usp.br/diversidade/pesquisadores-dedicados-a-linguistica-africana-estudam-impacto-no-brasil-de-hoje/'
-, 'Simpósio gratuito na USP reúne os primeiros docentes do Brasil a ocuparem cadeiras dedicadas ao estudo da linguística africana no país', '2024-08-26'),
-('Miguel Sarti', 'MEC lança estratégia para melhorar alfabetização', 'https://www.soportugues.com.br/secoes/new.php?indice=174'
-, 'Após a divulgação dos dados do Censo da Educação Básica 2015, se comprometeu a unir esforços de três programas do governo para garantir a alfabetização do ensino fundamental.', '2016-03-23'),
-('Miguel Sarti', 'Como usar o gerúndio corretamente sem virar gerundismo', 'https://exame.com/hub-faculdade-exame/como-usar-o-gerundio-corretamente-sem-virar-gerundismo-2/'
-, 'É correto usar o gerúndio quando se quer expressar uma ideia ou ação que ocorre no momento de outra no futuro', '2016-03-23'),
-('Miguel Sarti', 'Projeto estudantil estimula aprendizagem da língua portuguesa e exercício da cidadania', 'https://jornal.usp.br/diversidade/projeto-estudantil-estimula-aprendizagem-da-lingua-portuguesa-e-exercicio-da-cidadania/'
-, 'Nos dias 10, 11 e 12 agosto, será possível se inscrever no Projeto Redigir, curso que capacita estudantes a desenvolver habilidades de comunicação, além de incentivar o pensamento crítico sobre direitos e deveres sociais.', '2023-08-10'),
-('Miguel Sarti', 'Professora da USP Ribeirão oferece curso gratuito de português para imigrantes', 'https://jornal.usp.br/campus-ribeirao-preto/professora-da-usp-ribeirao-oferece-curso-gratuito-de-portugues-para-imigrantes/ua-portuguesa-e-exercicio-da-cidadania/'
-, 'As inscrições vão de 1º a 5 de agosto e o curso terá início dia 12 de agosto na Biblioteca Sinhá Junqueira, no centro de Ribeirão Preto.', '2023-07-20'),
-('Miguel Sarti', 'Quantos países falam português no mundo além do Brasil?', 'https://exame.com/pop/quantos-paises-falam-portugues-no-mundo-alem-do-brasil-2/'
-, 'Junho, dia 10, é o Dia da Língua Portuguesa no Brasil, mas estamos longe de ser o único país a falar a língua', '2023-07-20'),
-('Ana Zimmerman', 'Série do Jornal Hoje aborda a língua portuguesa coloquial falada nas ruas', 'https://g1.globo.com/jornal-hoje/noticia/2015/03/serie-do-jornal-hoje-fala-sobre-lingua-coloquial-falada-nas-ruas.html'
-, 'A língua que a gente fala aborda a língua falada em conversas espontâneas.
-Linguistas defendem que na comunicação falada não existe certo ou errado.', '2015-03-18'),
-('Miguel Sarti', 'Estudiosos discutem a língua portuguesa na educação, literatura e comunicação', 'https://www.soportugues.com.br/secoes/new.php?indice=174'
-, 'Evento de valorização da língua está recebendo trabalhos acadêmicos até 16 de agosto', '2019-07-30');
-
--- Inserções na Tabela de "Autores"
-INSERT INTO autores (nome, contratante) VALUES
-("Miguel Sarti", "SESI-SENAI"),
-("Marília Monitchele", "Veja"),
-("Laísa Dall'Agnol", "Veja"),
-("Wilson Ledo", "CNN"),
-("Lucas Pordeus León", "CNN"),
-("Mirella Cordeiro", "CNN"),
-("Ana Zimmerman", "G1"),
-("Camilla Costa", "BBC");
-
 -- Inserções na Tabela de "Simulados"
 INSERT INTO simulados (fase_ensino, numero_questoes) VALUES
-("Ensino Fundamental 1", "5"),
-("Ensino Fundamental 2", "5"),
-("Ensino Médio", "15");
+('Ensino Fundamental 1', '5'),
+('Ensino Fundamental 2', '5'),
+('Ensino Médio', '15');
 
 --------------------------------Consultas nas Tabelas--------------------------------
+SELECT I.nome AS Entrevistado, H.nome AS Entrevistador, E.tempo_de_duracao
+FROM entrevistas E
+JOIN entrevistados I
+ON I.entrevistado_id = E.entrevistado
+JOIN membros H
+ON H.membro_id = E.entrevistado;
